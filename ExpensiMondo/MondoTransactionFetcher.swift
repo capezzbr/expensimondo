683ed6d2295c537160132e96daad4e8587d3e0f7
Gilbert Jolly <gilbertj99@gmail.com>
2016-01-23 13:43:27 +0000

    Added oath

---
+++ ExpensiMondo/MondoTransactionFetcher.swift
@@ -0,0 +1,21 @@
+//
+//  MondoTransactionFetcher.swift
+//  ExpensiMondo
+//
+//  Created by Gilbert Jolly on 23/01/2016.
+//  Copyright © 2016 Gilbert Jolly. All rights reserved.
+//
+
+import Foundation
+
+// "https://api.getmondo.co.uk/transactions" \
+// "Authorization: Bearer $access_token" \
+// "account_id==$account_id"
+
+class MondoTransactionFetcher: NSObject {
+    
+    func fetchTransactions(){
+        
+    }
+    
+}
