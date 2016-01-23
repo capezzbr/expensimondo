3cfda99e6009622b4f799f833088c2ef0a8ec115
Gilbert Jolly <gilbertj99@gmail.com>
2016-01-23 12:48:40 +0000

    Connecting the Authenticatro to UI

--- ExpensiMondo/MasterViewController.m
+++ ExpensiMondo/MasterViewController.m
@@ -8,8 +8,11 @@
 
 #import "MasterViewController.h"
 #import "DetailViewController.h"
+#import "ExpensiMondo-Swift.h"
 
 @interface MasterViewController ()
+
+@property (nonatomic, strong) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,42 +20,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
-    self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    self.clearsSelectionOnViewWillAppear = self.splitViewController.isCollapsed;
+    
+    if (![MondoAuthenticator isLoggedIn]){
+        [[MondoAuthenticator instance] login:self callback:^(NSError * error) {
+            
+        }];
+    }
+    
     [super viewWillAppear:animated];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender {
-    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-        
-    // If appropriate, configure the new managed object.
-    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
-        
-    // Save the context.
-    NSError *error = nil;
-    if (![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-}
 
 #pragma mark - Segues
 
