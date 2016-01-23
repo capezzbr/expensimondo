ee9f7f42e4c4a81e3d14086c2f4156d9edf40527
Gilbert Jolly <gilbertj99@gmail.com>
2016-01-23 12:48:18 +0000

    Added assets

---
+++ ExpensiMondo/ExpensiMondo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ExpensiMondo-Bridging-Header.h
+//  ExpensiMondo
+//
+//  Created by Gilbert Jolly on 23/01/2016.
+//  Copyright © 2016 Gilbert Jolly. All rights reserved.
+//
+
+#ifndef ExpensiMondo_Bridging_Header_h
+#define ExpensiMondo_Bridging_Header_h
+
+
+#endif /* ExpensiMondo_Bridging_Header_h */
---
+++ ExpensiMondo/MondoAuthenticator.swift
@@ -0,0 +1,9 @@
+//
+//  MondoAuthenticator.swift
+//  ExpensiMondo
+//
+//  Created by Gilbert Jolly on 23/01/2016.
+//  Copyright © 2016 Gilbert Jolly. All rights reserved.
+//
+
+import Foundation
