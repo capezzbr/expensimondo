ae776e03a540e1803474f55cbca03a9f5128ae90
Bruno Capezzali <capezzbr@gmail.com>
2016-01-23 13:31:49 +0000

    ExpensifyAPI.m singleton structure

---
+++ ExpensiMondo/ExpensifyAPI.h
@@ -0,0 +1,13 @@
+//
+//  ExpensifyAPI.h
+//  ExpensiMondo
+//
+//  Created by Bruno Capezzali on 1/23/16.
+//  Copyright © 2016 Gilbert Jolly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExpensifyAPI : NSObject
+
+@end
---
+++ ExpensiMondo/ExpensifyAPI.m
@@ -0,0 +1,13 @@
+//
+//  ExpensifyAPI.m
+//  ExpensiMondo
+//
+//  Created by Bruno Capezzali on 1/23/16.
+//  Copyright © 2016 Gilbert Jolly. All rights reserved.
+//
+
+#import "ExpensifyAPI.h"
+
+@implementation ExpensifyAPI
+
+@end
