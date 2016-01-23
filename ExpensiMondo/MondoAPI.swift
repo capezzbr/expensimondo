55cb359867dca010c81ecf3918b7afefe5bcebf4
Gilbert Jolly <gilbertj99@gmail.com>
2016-01-23 16:14:37 +0000

    Added alamofire

--- ExpensiMondo/MondoAPI.swift
+++ ExpensiMondo/MondoAPI.swift
@@ -7,53 +7,47 @@
 //
 
 import Foundation
+import Alamofire
 
 
 public class MondoAPI : NSObject {
     
     static let instance = MondoAPI()
-    let manager = AFHTTPSessionManager(baseURL: NSURL(string:"https://api.getmondo.co.uk"))
+    let token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NTM4MjI2OTgsImlhdCI6MTQ1MzU2MzQ5OCwianRpIjoidG9rXzAwMDA5NFM3ZDAwRTRLZ2R5SlA2NUIiLCJ1aSI6InVzZXJfMDAwMDk0SzJBOWZ5eUNyZklyVGxrdiIsInYiOiIxIn0.ki-Br2aJQZsJPgXQu7eGQ9c1DimHuIJPYqWu4DjleAE"
     
     override init() {
-        manager.requestSerializer = AFHTTPRequestSerializer();
 
         super.init()
     }
     
     func fetchToken(){
-        let (username, password) = fetchCredentials()
+        let (username, password) = loadCredentials()
         
         let params = ["grant_type":"password",
                       "client_id":"oauthclient_000094RpteIGEnsZRmJrtJ",
                       "client_secret":"UuoNeRGv470bs0sCnpdWB7OqpCdcXbumna8OrISugyCjutS6zUqEqJy72cyO8hgwXkg+utcfBBdnFn6HAHde",
                       "username": username,
                       "password": password]
-        
-        
-        
-        manager.POST("oath2/token", parameters: nil, constructingBodyWithBlock: { (data) -> Void in
-            
-            for key in params.keys {
-                let value = params[key]! as String
-                let valueData = value.dataUsingEncoding(NSUTF8StringEncoding)
-                data.appendPartWithFormData(valueData!, name: key)
-            }
-            
-            }, success: { (task, response) -> Void in
-                print(response)
-            }) { (task, error) -> Void in
-                print(error)
-        }
     }
     
-    func fetchCredentials() -> (String, String){
+    func loadCredentials() -> (String, String){
         let path = NSBundle.mainBundle().pathForResource("Credentials", ofType: ".json")
         let data = NSData(contentsOfFile: path!)
         
         let dict = try! NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! NSDictionary
-        print(dict)
         let username = dict["username"] as! String
         let password = dict["password"] as! String
         return (username, password)
+    }
+    
+    
+    func fetchTransactions() {
+        
+        Alamofire.request(.GET, "https://api.getmondo.co.uk/transactions",
+            parameters: ["account_id":"acc_000094K2A9kwfl0leKMn7x"],
+            encoding: ParameterEncoding.URLEncodedInURL,
+            headers: ["Authorization" : token]).response { (request, response, data, error) -> Void in
+                let string = NSString(data: data!, encoding: NSUTF8StringEncoding)
+        }
     }
 }
--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '8.0'
 # Uncomment this line if you're using Swift
-# use_frameworks!
+use_frameworks!
 
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
@@ -9,5 +9,6 @@ platform :ios, '8.0'
 target 'ExpensiMondo' do
 
 pod 'AFNetworking', '~> 3.0'
+pod 'Alamofire', '~> 3.0'
 
 end
